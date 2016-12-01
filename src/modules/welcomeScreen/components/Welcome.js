1124f8a40ee5034bce16a299ecdbefb56ad02979
Bryan Guenther <bryan.guenther@freshgrade.com>
2016-12-01 10:48:01 -0800

    include login gif

--- src/modules/welcomeScreen/components/Welcome.js
+++ src/modules/welcomeScreen/components/Welcome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-// import { Card } from 'react-native-elements';
+import { Card } from 'react-native-elements';
 
 const Welcome = (props) => {
   console.log(props);
